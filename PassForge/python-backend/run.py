d5a3c9be99bb78a8b1053d14b7af0f77e52733b9
x1b1t0 <ivaanv23@gmail.com>
2025-02-14 12:55:21 +0100

    Delete PassForge directory

--- PassForge/python-backend/run.py
+++
@@ -1,6 +0,0 @@
-from app import create_app  # Importa la función para crear la aplicación
-
-app = create_app()  # Crea la aplicación Flask
-
-if __name__ == "__main__":
-    app.run(debug=True)  # Inicia el servidor en modo de desarrollo
